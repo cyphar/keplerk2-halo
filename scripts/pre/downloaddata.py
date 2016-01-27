cdc32252fb12eb1a8559f8201f9976a9917ba1be
Aleksa Sarai <cyphar@cyphar.com>
2016-01-27 16:56:11 +1100

    scripts: pre: downloaddata: support compressed data

--- scripts/pre/downloaddata.py
+++ scripts/pre/downloaddata.py
@@ -18,6 +18,7 @@
 import os
 import sys
 import json
+import zlib
 import argparse
 import requests
 
@@ -32,7 +33,8 @@ def fits_url(campaign, target, fits, **_):
 		return x - x%n
 	return ARCHIVE_URL % (campaign, lead(target, 1e5), lead(target % 1e5, 1e3), fits)
 
-def save_fits(url, ofile, chunk_size=1024):
+def save_fits(url, ofile, config, chunk_size=1024):
+	z = zlib.decompressobj(16 + zlib.MAX_WBITS)
 	r = requests.get(url, stream=True)
 	n = 0
 	for chunk in r.iter_content(chunk_size=chunk_size):
@@ -42,8 +44,14 @@ def save_fits(url, ofile, chunk_size=1024):
 				sys.stdout.write(".")
 				sys.stdout.flush()
 
+			if config.decompress:
+				chunk = z.decompress(chunk)
+
 			ofile.write(chunk)
 			ofile.flush()
+
+	tail = z.flush()
+	assert(len(tail) == 0)
 
 def save_data(path, config):
 	with open(os.path.join(path, DATA_JSON)) as fstars:
@@ -61,10 +69,14 @@ def save_data(path, config):
 				epic = json.load(fepic)
 				url = fits_url(**epic)
 
-			opath = os.path.join(path, star, target, epic["fits"] + ".gz")
+			opath = os.path.join(path, star, target, epic["fits"])
+
+			if not config.decompress:
+				opath += ".gz"
+
 			if config.clobber or not os.path.exists(opath):
 				with open(opath, "wb") as ofile:
-					save_fits(url, ofile)
+					save_fits(url, ofile, config)
 
 			sys.stdout.write(" OK\n")
 			sys.stdout.flush()
@@ -74,6 +86,8 @@ def main(path, config):
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Downloads the set of Kepler K2 Long Cadence Files given by a particular `data` metadata tree.")
+	parser.add_argument("-d", "--decompress", dest="decompress", action="store_const", const=True, default=True, help="Enable automatic decompression of data (default).")
+	parser.add_argument("-nd", "--no-decompress", dest="decompress", action="store_const", const=False, default=True, help="Disable automatic decompression of data.")
 	parser.add_argument("-c", "--clobber", dest="clobber", action="store_const", const=True, default=False, help="Enable clobbering of existing data.")
 	parser.add_argument("-nc", "--no-clobber", dest="clobber", action="store_const", const=False, default=False, help="Disable clobbering of existing data (default).")
 	parser.add_argument("path")
